b11944625b7f3dc2cba90b37fcd1071ae0e2715c
Cameron Thacker <cthacker@udel.edu>
2022-02-26 14:28:29 -0500

    [master] Refactored landing page

--- client/src/modules/LandingPage/LandingPage.tsx
+++ client/src/modules/LandingPage/LandingPage.tsx
@@ -1,37 +1,43 @@
 import React, { useState } from "react";
-import { Container, Button, Spinner } from "react-bootstrap";
+import { Container, Row, Col, Button, Spinner } from "react-bootstrap";
 import { useNavigate } from "react-router-dom";
 import styles from "./LandingPage.module.css";
 
 export const LandingPage = () => {
+  const navigate = useNavigate();
+  const [loading, setLoading] = useState<boolean>(false);
+  const navigateToLogin = () => {
+    setTimeout(() => {
+      navigate("/app/login");
+      setLoading(false);
+    }, 4200);
+    setLoading(true);
+  };
 
-    const navigate = useNavigate();
-    const [loading, setLoading] = useState<boolean>(false);
-    const navigateToLogin = () => {
-        setTimeout(() => {
-            navigate('/app/login');
-            setLoading(false);
-        }, 5000);
-        setLoading(true);
-    }
-    return (
-
-        <Container fluid className={`${styles.landing_page_container} ${styles.row_height}`}>
-            <div className={`${styles.landing_page_button}`}>
-                AGATHA PROJECT
-            </div>
-            <div className={`${styles.landing_page_button_div}`}>
-                {
-                    !loading ?
-                    <Button variant="outline-success" className={`${styles.landing_page_button}`} onClick={navigateToLogin}>
-                        Enter Project
-                    </Button>
-                    :
-                    <Spinner animation="border" variant="primary" />
-                }
-            </div>
-        </Container>
-
-    );
-
+  return (
+    <Container
+      fluid
+      className={`${styles.landing_page_container} ${styles.row_height}`}
+    >
+      <Row>
+        <Col>
+          <div className={`${styles.landing_page_header} ml-5`}>AGATHA PROJECT</div>
+          <div className={`${styles.landing_page_button_div}`}>
+            {!loading ? (
+              <Button variant="outline-success" onClick={navigateToLogin} className={`${styles.landing_page_button}`}>
+                Enter Project
+              </Button>
+            ) : (
+                <>
+                    Loading...
+                </>
+                // <div className={`${styles.landing_page_spinner}`}>
+                //     <Spinner animation="border" variant="primary"/>
+                // </div>
+            )}
+          </div>
+        </Col>
+      </Row>
+    </Container>
+  );
 };
